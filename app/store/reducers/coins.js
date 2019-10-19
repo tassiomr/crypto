9c96cf82cdba70250b7be46bc46a5178d1f07f9b
Tássio Rocha <tassiorocha@ioasys.com.br>
2019-10-19 19:46:43 -0300

    feat: create reducers coins

---
+++ app/store/reducers/coins.js
@@ -0,0 +1,12 @@
+const defaultState = {
+  coinsList: [],
+  isLoading: false,
+  coinDetails: undefined,
+};
+
+export default (state = defaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
