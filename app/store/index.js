c34e4f082f083b46d1b5b8258ab5a15d1ce4991b
Tássio Rocha <tassiorocha@ioasys.com.br>
2019-10-19 19:46:24 -0300

    feat: configure store

---
+++ app/store/index.js
@@ -0,0 +1,9 @@
+import {applyMiddleware, createStore} from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
+
+const middlewares = applyMiddleware(thunk);
+
+const store = createStore(reducers, middlewares);
+
+export default store;
