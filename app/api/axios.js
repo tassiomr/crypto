3645b99a45f002d5cef8974295a0530591c89d51
Tássio Rocha <tassiorocha@ioasys.com.br>
2019-10-20 15:09:46 -0300

    feat: init axios interceptors

---
+++ app/api/axios.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { END_POINTS } from './endpoints';
+
+axios.create({
+  baseURL: END_POINTS.BASE_URL,
+});
+
+axios.interceptors.request.use(
+  async (config) => {
+    console.log('config', config);
+  },
+
+  (error) => Promise.reject(error),
+);
+
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response.status === 401) {
+      const requestConfig = error.config;
+      return axios(requestConfig);
+    }
+    return Promise.reject(error);
+  },
+);
+
+export default axios;
