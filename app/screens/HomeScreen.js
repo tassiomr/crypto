f0ce10fb1aa8824e5230035adbb38d092afad120
Tássio Rocha <tassiorocha@ioasys.com.br>
2019-10-20 18:47:56 -0300

    feat: call list of coins and create list

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,25 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components/native';
+import { FlatList } from 'react-native-gesture-handler';
+import { Screen } from '../components/core/screen';
+import { getCoinsList } from '../store/useCases/coins';
+import { ListItem } from '../components/presentational/listItem';
+import { TextInput } from '../components/core/textInput';
 
-export const HomeScreen = (props) => (
-  <View>
-    <Text>Home</Text>
-    <Button onPress={() => props.navigation.navigate('Coin')}>
-      <Text>Go to screen 2</Text>
-    </Button>
-  </View>
-);
+export const HomeScreen = (props) => {
+  const dispatch = useDispatch();
+  const { coins } = useSelector((state) => state);
 
-const View = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-`;
-const Text = styled.Text`
-  color: ${(props) => props.theme.title};
-`;
+  useEffect(() => {
+    dispatch(getCoinsList());
+  }, []);
 
-const Button = styled.TouchableOpacity`
-  padding: ${(props) => props.theme.spaces.default}px;
-  background-color: ${(props) => props.theme.success};
+  return (
+    <Screen isLoading={coins.isLoading}>
+      <TextInput placeholder="seach by name" />
+
+      <FlatList
+        style={{ width: '100%' }}
+        initialNumToRender={10}
+        maxToRenderPerBatch={10}
+        data={coins.coinsList}
+        ItemSeparatorComponent={() => <Separator />}
+        renderItem={({ item }) => (
+          <ListItem
+            item={item}
+            onPress={() => props.navigation.navigate('Coin', { item })}
+          />
+        )}
+      />
+    </Screen>
+  );
+};
+
+const Separator = styled.View`
+  width: 100%;
+  height: 1;
+  background-color: ${(props) => props.theme.title};
 `;
