c152247647f42ace049bf3f04236b86d69c0e45f
Tássio Rocha <tassiorocha@ioasys.com.br>
2019-10-20 19:32:49 -0300

    feat: user can filter using name.

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,30 +1,53 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components/native';
 import { FlatList } from 'react-native-gesture-handler';
 import { Screen } from '../components/core/screen';
-import { getCoinsList } from '../store/useCases/coins';
+import { getCoinsList, filtredCoins } from '../store/useCases/coins';
 import { ListItem } from '../components/presentational/listItem';
 import { TextInput } from '../components/core/textInput';
+import { Text } from '../components/core/text';
 
 export const HomeScreen = (props) => {
   const dispatch = useDispatch();
   const { coins } = useSelector((state) => state);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     dispatch(getCoinsList());
   }, []);
 
+  useEffect(() => {
+    setData(coins.coinsList);
+  }, [coins.coinsList]);
+
+  useEffect(() => {
+    setData(coins.filtredCoins);
+  }, [coins.filtredCoins]);
+
+  const onChangeText = (text) => {
+    if (!text) {
+      setData(coins.coinsList);
+    }
+
+    dispatch(filtredCoins(text));
+  };
+
   return (
     <Screen isLoading={coins.isLoading}>
-      <TextInput placeholder="seach by name" />
+      <TextInput placeholder="seach by name" onChangeText={onChangeText} />
 
       <FlatList
         style={{ width: '100%' }}
         initialNumToRender={10}
         maxToRenderPerBatch={10}
-        data={coins.coinsList}
+        data={data}
         ItemSeparatorComponent={() => <Separator />}
+        ListEmptyComponent={() => (
+          <ListEmptyComponent>
+            <Text.Title text="search empty" />
+          </ListEmptyComponent>
+        )}
         renderItem={({ item }) => (
           <ListItem
             item={item}
@@ -40,4 +63,10 @@ const Separator = styled.View`
   width: 100%;
   height: 1;
   background-color: ${(props) => props.theme.title};
+`;
+
+const ListEmptyComponent = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
 `;
