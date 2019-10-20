016353cb81a1771af6fcf19187bc9d7b7d07c855
Tássio Rocha <tassiorocha@ioasys.com.br>
2019-10-20 15:09:12 -0300

    improvement: pass BASE_URL in  END_POINTS

--- app/api/endpoints.js
+++ app/api/endpoints.js
@@ -1,6 +1,7 @@
 const BASE_URL = 'https://api.coingecko.com/api/v3';
 
 export const END_POINTS = {
+  BASE_URL,
   PING: `${BASE_URL}/ping/`,
   COIN_DETAIL: `${BASE_URL}/coins/`,
   COINS_LIST: `${BASE_URL}/conins/list/`,
