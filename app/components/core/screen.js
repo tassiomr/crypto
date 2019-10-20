aaa7242d0703049fc8795c0f26fb9225c022217a
Tássio Rocha <tassiorocha@ioasys.com.br>
2019-10-20 18:49:56 -0300

    feat: add loading in screen

--- app/components/core/screen.js
+++ app/components/core/screen.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import styled from 'styled-components/native';
-import PropTypes from 'prop-types';
-import { Text } from './text';
-import { Toast } from './toats';
 
-export const Screen = (props) => (
-  <View>
-    <Text.Title text="Screen" />
-    {props.children}
-    <Toast />
-  </View>
-);
+import PropTypes from 'prop-types';
+import { Toast } from './toats';
+import { Indicator } from './indicator';
+
+export const Screen = (props) => {
+  const { isLoading, children } = props;
+
+  return (
+    <View isLoading={isLoading}>
+      {!isLoading ? <>{children}</> : <Indicator isLoading={isLoading} large />}
+      <Toast />
+    </View>
+  );
+};
 
 Screen.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
   children: PropTypes.element.isRequired,
 };
 
 const View = styled.SafeAreaView`
   flex: 1;
   background-color: ${(props) => props.theme.backgroundColor};
+  justify-content: ${(props) => (props.isLoading ? 'center' : 'flex-start')};
+  align-items: ${(props) => (props.isLoading ? 'center' : 'flex-start')};
+  width: 100%;
 `;
